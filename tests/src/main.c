7310a9ac7c5722a39858eb3d6b1b6184fe7a37f3
Inaki Segura <isegura@hawk.iit.edu>
2020-05-04 18:49:43 -0500

    Added queue test

--- tests/src/main.c
+++ tests/src/main.c
@@ -3,12 +3,10 @@
 
 #include <simplelib/simple_queue.h>
 #include <simplelib/simple_stack.h>
-#include <simplelib/simple_queue.h>
-
 
 #define streq(a, b) strcmp(a, b) == 0
 
-int stack_add_test()
+int stack_push_pop_test()
 {
     int size;
     fscanf(stdin, "%d\n", &size);
@@ -33,14 +31,43 @@ int stack_add_test()
     return is_stack_empty(stack);
 }
 
+int queue_push_pop_test()
+{
+    int size;
+    fscanf(stdin, "%d\n", &size);
+
+    int input[size];
+    for (int i = 0; i < size; i++)
+    {
+        fscanf(stdin, "%d\n", input + i);
+    }
+
+    simple_queue_t* queue = create_queue();
+    for (int i = 0; i < size; i++)
+    {
+        push_queue(queue, input[i]);
+    }
+    
+    for (int i = 0; i < size; i++)
+    {
+        fprintf(stdout, "%d\n", deque(queue));
+    }
+
+    return is_stack_empty(queue);
+}
+
 int main(int argc, char** argv)
 {
     char test[32];
     fscanf(stdin, "%s\n", test);
 
-    if (streq(test, "stack-add"))
+    if (streq(test, "stack-push-pop"))
     {
-        return stack_add_test();
+        return stack_push_pop_test();
+    }
+    else if (streq(test, "queue-push-deque"))
+    {
+        return queue_push_pop_test();
     }
 
     return 1;
